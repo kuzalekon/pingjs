662c34171a3f9b598d97afeaab0c10f3a1fd0704
Кузнецов Алексей <C:\Users\kuznetsov.aleksey\AppData\Roaming\The Bat!>
2018-04-09 15:01:15 +0300

    Moving to ES6

--- ping.js
+++ ping.js
@@ -3,139 +3,95 @@
 
     // RequireJS and other-like
     if (typeof define === 'function' && define.amd)
-        define(name, function () { return init(window, document) });
+        define(name, function () { return init(document) });
     // CommonJS and other-like
     else if (typeof module === 'object' && module.exports)
-        module.exports = init(window, document);
+        module.exports = init(document);
     // Other environments (browser & etc)
     else
-        root[name] = init(window, document);
+        root[name] = init(document);
 
-}('ping', this, function (win, doc) {
+}('ping', this, function (doc) {
 
-    var ONLINE  = 'online';
-    var OFFLINE = 'offline';
+    function pingByFetch(url, start) {
+        return new Promise((resolve, reject) => {
+            if (typeof fetch !== 'function')
+                return reject(new Error('no such method'));
 
-    return function ping() {
-        if (arguments.length < 1)
-            throw new TypeError('too few arguments');
+            fetch(url.href, { method: 'HEAD', cache: 'no-cache', mode: 'no-cors' }).then(() => {
+                resolve({ host: url.host, status: 'online', time: (Date.now() - start) });
+            }, () => {
+                resolve({ host: url.host, status: 'offline', time: (Date.now() - start) });
+            });
+        });
+    }
 
-        var host = '';
-        var timeout = 60 * 1000;
-        var faviconPath = 'favicon.ico';
-        if (typeof arguments[0] === 'string') {
-            host = arguments[0];
-        }
-        else {
-            var options = arguments[0];
-            host = options.host || host;
-            timeout = options.timeout || timeout;
-            faviconPath = options.favicon || faviconPath;
-        }
+    function pingByScript(url, start) {
+        return new Promise((resolve, reject) => {
+            const onerror = (event) => {
+                doc.head.removeChild(event.target);
+                reject(new Error('cannot check status'));
+            }
 
-        return new Promise(function (resolve, reject) {
-            var url = doc.createElement('a');
+            const onload = (event) => {
+                doc.head.removeChild(event.target);
+                resolve({ host: url.host, status: 'online', time: (Date.now() - start) });
+            }
+
+            url.search = `_=${start}`;
+
+            const script = doc.createElement('script');
+            script.onerror = onerror;
+            script.onload = onload;
+            script.type = 'application/javascript'; // Required in HTML4
+            script.src = url.href;
+            doc.head.appendChild(script);
+        });
+    }
+
+    function pingByFavicon(url, favicon, start) {
+        return new Promise((resolve, reject) => {
+            url.pathname = favicon;
+            url.search = `_=${start}`;
+
+            const image = new Image();
+            image.onerror = () => resolve({ host: url.host, status: 'offline', time: (Date.now() - start) });
+            image.onload = () => resolve({ host: url.host, status: 'online', time: (Date.now() - start) });
+            image.src = url.href;
+        });
+    }
+
+    return function ping(host, options = {}) {
+        return new Promise((resolve, reject) => {
+            const url = doc.createElement('a');
             url.href = host;
             url.pathname = '';
             url.search = '';
             url.hash = '';
-            // Check for insecure request
-            if (win.location.protocol === 'https:' && url.protocol === 'http:')
+            if (location.protocol === 'https:' && url.protocol === 'http:')
                 return reject(new Error('insecure request'));
 
-            var startTime = 0;
-            var isTimeout = false;
-            var timerId = win.setTimeout(function onTimeout() {
-                isTimeout = true;
-                reject(new Error('timeout occured'));
-            }, timeout);
+            const timeout = options.timeout || 60 * 1000;
+            const favicon = options.favicon || 'favicon.ico';
+            const timerid = setTimeout(() => resolve({ host: host, status: 'timeout', time: timeout }), timeout);
 
-            pingByFetch(url.href);
-
-            function pingByFetch(host) {
-                if (typeof win.fetch !== 'function')
-                    return pingByScript(host);
-
-                startTime = Date.now();
-                win.fetch(host, { method: 'HEAD', mode: 'no-cors', cache: 'no-cache' })
-                    .then(function onFetchResolved() {
-                        if (!isTimeout) {
-                            win.clearTimeout(timerId);
-                            resolve({
-                                host: host,
-                                status: ONLINE,
-                                time: Date.now() - startTime
-                            });
-                        }
-                    })
-                    .catch(function onFetchRejected() {
-                        if (!isTimeout) {
-                            win.clearTimeout(timerId);
-                            resolve({
-                                host: host,
-                                status: OFFLINE,
-                                time: Date.now() - startTime
-                            });
-                        }
+            pingByFetch(url, Date.now()).then((result) => {
+                clearTimeout(timerid);
+                resolve(result);
+            }, (error) => {
+                pingByScript(url, Date.now()).then((result) => {
+                    clearTimeout(timerid);
+                    resolve(result);
+                }, (error) => {
+                    pingByFavicon(url, favicon, Date.now()).then((result) => {
+                        clearTimeout(timerid);
+                        resolve(result);
+                    }, (error) => {
+                        clearTimeout(timerid);
+                        reject(error);
                     });
-            }
-
-            function pingByScript(host) {
-                startTime = Date.now();
-
-                var url = doc.createElement('a');
-                url.href = host;
-                url.search = '?_=' + Date.now();
-
-                var script = document.createElement('script');
-                script.type = 'application/javascript'; // Required in HTML4
-                script.src = url.href;
-                script.onload = script.onerror = function onScriptResponse(event) {
-                    doc.head.removeChild(event.target);
-                    if (!isTimeout) {
-                        win.clearTimeout(this.__timer);
-                        if (event.type === 'load')
-                            resolve({
-                                host: host,
-                                status: ONLINE,
-                                time: Date.now() - startTime
-                            });
-                        else
-                            pingByFavicon(host);
-                    }
-                }
-
-                doc.head.appendChild(script);
-            }
-
-            function pingByFavicon(host) {
-                startTime = Date.now();
-
-                var url = doc.createElement('a');
-                url.href = host;
-                url.pathname = faviconPath;
-                url.search = '?_=' + Date.now();
-
-                var favicon = new Image();
-                favicon.src = url.href;
-                favicon.onload = favicon.onerror = function onFaviconResponse(event) {
-                    if (!isTimeout) {
-                        win.clearTimeout(timerId);
-                        resolve(event.type === 'load' ?
-                            {
-                                host: host,
-                                status: ONLINE,
-                                time: Date.now() - startTime
-                            } :
-                            {
-                                host: host,
-                                status: OFFLINE,
-                                time: Date.now() - startTime
-                            });
-                    }
-                }
-            }
-
+                });
+            });
         });
     }
 
