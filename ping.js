76fdecbf9867f4083631707f7ee2b248dadaecf1
Kuznetsov Aleksey <kyznecov.alexey@gmail.com>
2018-06-22 13:08:17 +0300

    Correction of bugs and up-dating of comments

--- ping.js
+++ ping.js
@@ -13,21 +13,32 @@
 
 }('ping', this, function (doc) {
 
+    'use strict';
+
+    /**
+     * Checks availability of host
+     *
+     * @function ping
+     * @global
+     * @async
+     * @param {String} host Host address for check
+     * @param {Object} options Additional options of check such as timeout value or favicon path
+     */
     const ping = (host, options = {}) => {
         return new Promise((resolve, reject) => {
             if (!host || typeof host !== 'string')
-                reject(new Error('\"host\" must be a string'));
+                reject(new TypeError('\"host\" must be a string'));
 
-            // Setup a request timeout to 1 minute by default
-            const timeout = options.timeout || 60 * 1000;
+            // Setup a request timeout to 30 seconds by default
+            const timeout = options.timeout || 30 * 1000;
             // Setup a favicon path by default (most popular is "/favicon.ico")
             const favicon = options.favicon || 'favicon.ico'
 
             if (!timeout || typeof timeout !== 'number')
-                reject(new Error('\"timeout\" must be a number'));
+                reject(new TypeError('\"timeout\" must be a number'));
 
             if (!favicon || typeof favicon !== 'string')
-                reject(new Error('\"favicon\" must be a string'));
+                reject(new TypeError('\"favicon\" must be a string'));
 
             // Use HTMLAnchorElement as a cross-browser wrapper of URL object
             const url = doc.createElement('a');
@@ -47,19 +58,19 @@
                 resolve({ host: host, status: 'timeout', time: timeout });
             }, timeout);
 
-             // Tries to check availability of host by request a favicon.
-             // If the "onload" handler called - the host is available,
-             // and if called "onerror" handler - the host is unavailable.
+            // Tries to check availability of host by request a favicon.
+            // If the "onload" handler called - the host is available,
+            // and if called "onerror" handler - the host is unavailable.
             const pingByFavicon = () => {
-                url.pathname = favicon;
+                url.pathname = favicon; // Add favicon path to host URL
                 url.search = `?_=${+new Date()}`; // Disable cache
 
-                const start = +new Date(); // Check start time
+                const start = +new Date(); // Start time of check
                 // If path to favicon is specified we believe that accuracy of method
                 // is high (because user knowns path), otherwise we believe accuracy is low
                 const result = { host: host, accuracy: options.favicon ? 'high' : 'low' };
 
-                // Function for processing the result of favicon loading
+                // Callback used by "onload" and "onerror" handlers of img element
                 const callback = (event) => {
                     clearTimeout(timerId);
 
@@ -75,22 +86,30 @@
                 image.src = url.href;
             }
 
-            // Tries to check availability of host by request resource as script.
+            // Tries to check availability of host by request index page as script.
             // If the "onload" handler called - the host is available,
             // and if called "onerror" handler - the host is unavailable.
             const pingByScript = () => {
                 url.search = `?_=${+new Date()}`; // Disable cache
 
-                const start = +new Date(); // Check start time
+                const start = +new Date(); // Start time of check
                 const result = { host: host, accuracy: 'medium' };
                 const head = doc.getElementsByTagName('head')[0];
 
-                // Function for processing the result of script loading
+                /**
+                 * Callback used by "onload" and "onerror" handlers of script element
+                 *
+                 * @callback
+                 * @param {Event} event
+                 */
                 const callback = (event) => {
                     clearTimeout(timerId);
                     head.removeChild(event.target);
 
-                    result.status = event.type === 'load' ? 'available' : 'unavailable';
+                    if (event.type === 'error')
+                        return pingByFavicon();
+
+                    result.status = 'available';
                     result.time = +new Date() - start;
                     resolve(result);
                 }
@@ -110,7 +129,7 @@
             // otherwise - the host is unavailable.
             const pingByFetch = () => {
                 const init = { method: 'head', mode: 'no-cors' /* required */, cache: 'no-cache' };
-                const start = +new Date(); // Check start time
+                const start = +new Date(); // Start time of check
                 const result = { host: host, accuracy: 'high' };
 
                 fetch(url.href, init).then(() => {
@@ -120,13 +139,13 @@
                 });
             }
 
-            // If our page is loaded via HTTPS, and the resources of the host being tested
-            // are loading via HTTP, then the only way to check is to request favicon
+            // If our page is loaded via HTTPS, but the tested host can be loaded
+            // only via HTTP, then the only way to check is to request favicon
             if (location.protocol === 'https:' && url.protocol === 'http:')
                 return pingByFavicon();
 
-            // In older browsers (where there is no method "fetch") we can only check host
-            // availability by requesting resource as script or request a favicon
+            // In older browsers (where there is no method "fetch") we only can check host
+            // availability by requesting index page as script and/or favicon
             if (!fetch)
                 return pingByScript();
 
